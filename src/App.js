ca6fc89939824db7673718a1dbac37bb441c5db7
seemsss20 <seemadhamgunde2020@gmail.com>
2021-04-28 01:20:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <PathfindingVisualizer />
+        </div>
+    );
+}
+
+export default App;
